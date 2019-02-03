c0958201c58731f30c294ed5969ac426ec83daad
Mwatyar <usui0303way@yahoo.co.jp>
2019-02-04 00:38:29 +0900

    更新2

--- drivertest.py
+++ drivertest.py
@@ -2,10 +2,13 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://www.catalog.update.microsoft.com/Home.aspx')
+driver.get('https://www.catalog.update.microsoft.com/Search.aspx?q=Microsoft Excel 2016 (KB4461600) 64 ビット版 の更新プログラム')
+time.sleep(3)
+driver.find_element_by_xpath('//*[@class="resultsbottomBorder resultsButtonWidth"]').click()
 time.sleep(5)
-search_box = driver.find_element_by_name("q")
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5)
+driver.switch_to_window(driver.window_handles[1]) 
+driver.find_element_by_tag_name('a').click()
+time.sleep(10)
+
+
 driver.quit()
